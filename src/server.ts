6864d5c0ee828e1fd000b9f7401ce3ecaafab481
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-12-13 01:18:16 +0600

    unhandledRejection and uncaughtException added

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,13 @@ import configs from './configs';
 import mongoConnect from './configs/dbConnect';
 import catchEnvValidation from './utils/catchEnvValidation';
 
+// create server
+const server = createServer(app);
+
 const main = async () => {
   try {
     // check env validation
     await catchEnvValidation();
-
-    // create server
-    const server = createServer(app);
 
     // database connection with mongoose(mongodb)
     mongoConnect();
@@ -22,8 +22,23 @@ const main = async () => {
       console.log(`Hello Boss! I am listening at http://localhost:${port}`);
     });
   } catch (error) {
-    console.log(`Server error: ${error}`);
+    console.log(`Server error: ${(error as Error).message}`);
   }
 };
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log('😈 unhandledRejection is detected, shutting down the process..');
+  if (server) {
+    return server.close(() => {
+      process.exit(1);
+    });
+  }
+  return process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log('😈 uncaughtException is detected, shutting down the process..');
+  process.exit(1);
+});
